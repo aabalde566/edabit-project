a22b45f3e2733fe73a3877a895b59f5c20dbe336
aabalde566 <abdouazizbalde566@gmail.com>
2026-03-25 16:58:09 +0000

    Add files via upload

---
+++ exercices.js
@@ -0,0 +1,47 @@
+function calcAge(age) {
+  return age * 365;
+}
+
+function convert(hours) {
+  return hours * 3600;
+}
+
+function triArea(base, height) {
+  return (base * height) / 2;
+}
+
+function addition(a, b) {
+  return a + b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
+
+function lessThanOrEqualToZero(num) {
+  return num <= 0;
+}
+
+function findPerimeter(length, width) {
+  return 2 * (length + width);
+}
+
+function addition(num) {
+  return num + 1;
+}
+
+function points(twoPointers, threePointers) {
+  return (twoPointers * 2) + (threePointers * 3);
+}
+
+function giveMeSomething(a) {
+  return "something " + a;
+}
+
+function lessThan100(a, b) {
+  return (a + b) < 100;
+}
+
+function sumPolygon(n) {
+  return (n - 2) * 180;
+}
